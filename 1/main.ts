0b13210ec87cf578948f9fb40ed764d4880b7499
muminovbob <bobomurodm2004@gmail.com>
2024-12-03 21:04:30 +0500

    1 solved

---
+++ 1/main.ts
@@ -0,0 +1,21 @@
+let sum = 0;
+const a:number[] = [];
+const b:number[] = [];
+
+const input = await Deno.readTextFile("input.txt");
+const pairs = input.split("\n");
+
+pairs.forEach((x)=>{
+  const pair = x.split("   ");
+  a.push(Number(pair[0]));
+  b.push(Number(pair[1]));
+});
+
+a.sort();
+b.sort();
+
+for(let i = 0; i<a.length; i++){
+  sum += Math.abs(a[i] - b[i]);
+}
+
+console.log(sum);
